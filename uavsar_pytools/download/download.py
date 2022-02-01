c245a3b58ed9133f8cf23b96a988fd9d7972e2cc
ZachKeskinen <zachkeskinen@gmail.com>
2022-02-01 11:30:28 -0700

    testing

--- uavsar_pytools/download/download.py
+++ uavsar_pytools/download/download.py
@@ -87,10 +87,14 @@ def download_InSAR(url, output_dir, ann = False):
                 slc_pol = [pol for pol in pols if (pol in url)]
                 if len(slc_pol) == 1:
                     url = url.replace(slc_pol[0], '')
+
                 if ext == 'grd':
-                    url = url.replace('.grd','')
+                    if len(basename(url).split('.')) == 2:
+                        url = url.replace('.grd','.ann')
+                    if len(basename(url).split('.')) == 3:
+                        url = url.replace('.grd','')
                     ext = url.split('.')[-1]
-                ann_url = url.replace(f'{ext}', 'ann')
+                ann_url = url.replace(f'.{ext}', '.ann')
                 log.debug(f'Annotation url: {ann_url}')
 
             else:
@@ -104,37 +108,3 @@ def download_InSAR(url, output_dir, ann = False):
                     stream_download(ann_url, ann_local)
                 else:
                     log.info(f'{ann_local} already exists, skipping download!')
-
-
-def main():
-    ## Testing ASF
-    #url = 'https://unzip.asf.alaska.edu/INTERFEROMETRY_GRD/UA/grmesa_27416_21011-010_21016-002_0021d_s01_L090_01_int_grd.zip/grmesa_27416_21011-010_21016-002_0021d_s01_L090HH_01.cor.grd'
-    #url = 'https://datapool.asf.alaska.edu/INTERFEROMETRY_GRD/UA/lowman_05208_21019-019_21021-007_0006d_s01_L090_01_int_grd.zip'
-    # url = 'https://datapool.asf.alaska.edu/INC/UA/Rosamd_35012_21067_013_211124_L090_CX_01.inc'
-    # url = 'https://datapool.asf.alaska.edu/SLOPE/UA/Rosamd_35012_21067_013_211124_L090_CX_01.slope'
-    # url = 'https://unzip.asf.alaska.edu/COMPLEX/UA/Rosamd_35012_21067_013_211124_L090_CX_01_mlc.zip/Rosamd_35012_21067_013_211124_L090_CX_01.ann'
-    #url = 'https://unzip.asf.alaska.edu/STOKES/UA/Rosamd_35012_21067_013_211124_L090_CX_01_stokes.zip/Rosamd_35012_21067_013_211124_L090_CX_01.dat'
-    #url = 'https://datapool.asf.alaska.edu/PAULI/UA/Rosamd_35012_21067_013_211124_L090_CX_01_pauli.tif'
-    #url = 'https://unzip.asf.alaska.edu/COMPLEX/UA/Rosamd_35012_21067_013_211124_L090_CX_01_mlc.zip/Rosamd_35012_21067_013_211124_L090VVVV_CX_01.mlc'
-
-    ## Testing JPL
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090HHHH_CX_01.mlc'
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.dat'
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090HHVV_CX_01.grd' ##didnt work
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.inc'
-    url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.slope'
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.hgt'
-    #url = 'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.ann'
-    # url =  'http://uavsar.asfdaac.alaska.edu/UA_peeler_13711_19084_020_191220_L090_CX_01/peeler_13711_19084_020_191220_L090_CX_01.mlc'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20008-000_20013-000_0007d_s01_L090_02/alamos_35915_20008-000_20013-000_0007d_s01_L090HH_02.cor'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20008-000_20013-000_0007d_s01_L090_02/alamos_35915_20008-000_20013-000_0007d_s01_L090HH_02.amp2.grd'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20008-000_20013-000_0007d_s01_L090_02/alamos_35915_20008-000_20013-000_0007d_s01_L090HH_02.ann'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20013_000_200226_L090_CX_01/alamos_35915_20013_000_200226_L090VVVV_CX_01.mlc'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20013_000_200226_L090_CX_01/alamos_35915_20013_000_200226_L090HHHV_CX_01.grd'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20013_000_200226_L090_CX_01/alamos_35915_20013_000_200226_L090_CX_01.inc'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20013_000_200226_L090_CX_01/alamos_35915_20013_000_200226_L090_CX_01.slope'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20008-000_20013-000_0007d_s01_L090_02/alamos_35915_20008-000_20013-000_0007d_s01_L090HH_02.amp1'
-    # url = 'http://uavsar.asfdaac.alaska.edu/UA_alamos_35915_20013_000_200226_L090_CX_01/alamos_35915_20013_000_200226_L090_CX_01.ann'
-    download_InSAR(url, '../../data/', ann = True)
-
-main()
