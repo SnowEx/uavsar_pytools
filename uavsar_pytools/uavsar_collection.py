5980189f5042d77babdaaec3e5158029dc08ef51
Zach Keskinen <zachkeskinen@gmail.com>
2022-04-30 18:53:04 -0600

    adding inc functionality to uasvarcollection

--- uavsar_pytools/uavsar_collection.py
+++ uavsar_pytools/uavsar_collection.py
@@ -1,9 +1,13 @@
 import os
+from os.path import basename, join, expanduser
+from glob import glob
+import shutil
 import logging
 import asf_search as asf
 import pandas as pd
 
 from uavsar_pytools.uavsar_scene import UavsarScene
+from uavsar_pytools.uavsar_image import UavsarImage
 
 log = logging.getLogger(__name__)
 logging.basicConfig()
@@ -22,21 +26,22 @@ class UavsarCollection():
         pols (list): Do you want only certain polarizations? [Default = all available]
         dates (list): List of 1: start date and 2: end date to constrain collection results.
         low_ram (bool): decimates by a factor of 100 the arrays to conserve memory. [Default = True]
+        inc (bool): download incidence angle as well? [Default = False]
 
     Methods:
         collection_to_tiffs(): Main method. Finds all Uavsar Images in the collection and downloads, converts them to GeoTiffs.
+        find_urls() Finds all urls and returns thems as .results to the object. Each .result has a .properties property it inherits from asf_search.
     """
 
-    Scenes = []
-
-    def __init__(self, collection, work_dir, overwrite = False, clean = True, debug = False, pols = None, dates = None, low_ram = True):
+    def __init__(self, collection, work_dir, overwrite = False, clean = True, debug = False, pols = None, dates = None, low_ram = True, inc = False):
         self.collection = collection
-        self.work_dir = work_dir
+        self.work_dir = expanduser(work_dir)
         self.overwrite = overwrite
         self.clean = clean
         self.debug = debug
         self.pols = pols
         self.low_ram = low_ram
+        self.inc = inc
         if pols:
             pols = [pol.upper() for pol in pols]
             if set(pols).issubset(['VV','VH','HV','HH']):
@@ -65,12 +70,24 @@ class UavsarCollection():
 
     def results_to_tiffs(self):
         for result in self.results:
-            url = result.properties['url']
+            prop = result.properties
+            url = prop['url']
             log.info(f'Starting on: {url}')
             scene = UavsarScene(url = url, work_dir= self.work_dir, pols = self.pols, clean = self.clean, low_ram=self.low_ram)
             scene.url_to_tiffs()
             d1 = scene.images[0]['description']['start time of acquisition for pass 1']['value']
             d2 = scene.images[0]['description']['start time of acquisition for pass 2']['value']
+            if self.inc:
+                inc_res = asf.search(platform = 'UAVSAR',
+                        processingLevel = (['INC']),
+                        campaign = self.collection,
+                        frame= int(prop['frameNumber']),
+                        relativeOrbit= int(prop['pathNumber']),
+                        start= prop['startTime'],
+                        end = prop['stopTime'])[0]
+                url_dir = join(self.work_dir, basename(url).split('.')[0])
+                inc_img = UavsarImage(inc_res.properties['url'], join(self.work_dir, url_dir), clean = True)
+                inc_img.url_to_tiff()
             print(f'Completed {d1} to {d2}')
 
     def collection_to_tiffs(self):
