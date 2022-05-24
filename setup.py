a0dc66ee161b29e21859817e6fa2046528a0f1c3
Zach Keskinen <zachkeskinen@gmail.com>
2022-05-24 12:21:53 -0600

    doi versioning

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/SnowEx/uavsar_pytools'
 EMAIL = 'zachkeskinen@gmail.com'
 AUTHOR = 'Zach Keskinen and Jack Tarricone'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.4.9'
+VERSION = '0.5.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
